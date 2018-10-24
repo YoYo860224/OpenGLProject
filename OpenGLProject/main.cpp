994f00361a08f2d934a5a567f4d1e231a28c1185
YoCheng <xyox860224@hotmail.com>
2018-10-24 13:03:11 +0800

    Do 10 times for specific angle.

--- OpenGLProject/main.cpp
+++ OpenGLProject/main.cpp
@@ -14,17 +14,42 @@
 
 #define WINDOW_WIDTH 1024
 #define WINDOW_HEIGHT 768
-#define POINT_FARZ -200
-#define POINT_NEARZ 500
+#define VIEWPOINT_FARZ -200
+#define VIEWPOINT_NEARZ 500
+#define DEPTH_FARZ -100
+#define DEPTH_NEARZ 200
 
 using namespace std;
 
 typedef OpenMesh::PolyMesh_ArrayKernelT<> MyMesh;
 MyMesh mesh;
+string plyFilePath;
+string plyFileName;
 
 int drawWhat = 0;
+int drawIndex = 0;
+
+float rotateAngle[10][2] = { 
+	{ 0, 0 },
+	{ 0, 30 },
+	{ 0, -30 },
+	{ 0, 90 },
+	{ 0, -90},
+	{ 30, 0 },
+	{ 30, 45 },
+	{ 30, -45 },
+	{ -30, 30 },
+	{ -30, -30 },
+};
+
+void rotateMesh(MyMesh *thisMesh, double xAngle, double yAngle, double zAngle);
 
 void drawFunc(void) {
+	// rotateMesh
+	MyMesh mesh1 = MyMesh(mesh);
+	rotateMesh(&mesh1, rotateAngle[drawIndex][0], rotateAngle[drawIndex][1], 0);
+
+	// start to draw
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
 	glPushMatrix();
@@ -32,27 +57,27 @@ void drawFunc(void) {
 
 	if (drawWhat == 0) {
 		// Draw RGB
-		for (MyMesh::FaceIter f_it = mesh.faces_begin(); f_it != mesh.faces_end(); ++f_it)
+		for (MyMesh::FaceIter f_it = mesh1.faces_begin(); f_it != mesh1.faces_end(); ++f_it)
 		{
-			for (MyMesh::FVIter fv_it = mesh.fv_begin(*f_it); fv_it != mesh.fv_end(*f_it); ++fv_it)
+			for (MyMesh::FVIter fv_it = mesh1.fv_begin(*f_it); fv_it != mesh1.fv_end(*f_it); ++fv_it)
 			{
-				glColor3ubv(mesh.color(*fv_it).data());
-				glVertex3fv(mesh.point(*fv_it).data());
+				glColor3ubv(mesh1.color(*fv_it).data());
+				glVertex3fv(mesh1.point(*fv_it).data());
 			}
 		}
 	}
 	else if (drawWhat == 1) {
 		// Draw D
-		for (MyMesh::FaceIter f_it = mesh.faces_begin(); f_it != mesh.faces_end(); ++f_it)
+		for (MyMesh::FaceIter f_it = mesh1.faces_begin(); f_it != mesh1.faces_end(); ++f_it)
 		{
-			for (MyMesh::FVIter fv_it = mesh.fv_begin(*f_it); fv_it != mesh.fv_end(*f_it); ++fv_it)
+			for (MyMesh::FVIter fv_it = mesh1.fv_begin(*f_it); fv_it != mesh1.fv_end(*f_it); ++fv_it)
 			{
 				// our side is positive
-				double depthZ = mesh.point(*fv_it).data()[2];
-				double val = (depthZ - POINT_FARZ) / (POINT_NEARZ - POINT_FARZ);
+				double depthZ = mesh1.point(*fv_it).data()[2];
+				double val = (depthZ - DEPTH_FARZ) / (DEPTH_NEARZ - DEPTH_FARZ);
 
 				glColor3d(val, val, val);
-				glVertex3fv(mesh.point(*fv_it).data());
+				glVertex3fv(mesh1.point(*fv_it).data());
 			}
 		}
 	}
@@ -60,16 +85,24 @@ void drawFunc(void) {
 	glPopMatrix();
 	glFlush();
 
+	// save
+	string str;
 	if (drawWhat == 0) {
-		char name[10] = "rgb.bmp";
-		SaveScreenShot(name, WINDOW_WIDTH, WINDOW_HEIGHT);
-		drawWhat++;
+		str = plyFileName + "_" + to_string(drawIndex) + "_rgb.bmp";
+		drawWhat = 1;
 	}
 	else if (drawWhat == 1) {
-		char name[10] = "d.bmp";
-		SaveScreenShot(name, WINDOW_WIDTH, WINDOW_HEIGHT);
-		exit(0);
+		str = plyFileName + "_" + to_string(drawIndex) + "_d.bmp";
+		drawWhat = 0;
+		drawIndex++;
+		if (drawIndex >= 10)
+			exit(0);
 	}
+
+	char *name = new char[str.length() + 1];
+	strcpy(name, str.c_str());
+	SaveScreenShot(name, WINDOW_WIDTH, WINDOW_HEIGHT);
+	delete[] name;
 }
 
 void rotateMesh(MyMesh *thisMesh, double xAngle, double yAngle, double zAngle) {
@@ -104,10 +137,22 @@ void rotateMesh(MyMesh *thisMesh, double xAngle, double yAngle, double zAngle) {
 	}
 }
 
+string SplitFilename(const std::string& str) {
+	unsigned found = str.find_last_of("/\\");
+	return str.substr(found + 1);
+}
+
 int main(int argc, char** argv)
 {
-	string plyFilePath = "D:\\AllProjects\\Cpp\\OpenGLProject\\face.ply";
-	
+	// argv[0]: this program name
+	// argv[1]: file_name
+
+	plyFilePath = "D:\\AllProjects\\Cpp\\OpenGLProject\\face.ply";
+	if (argc >= 2)
+		plyFilePath = argv[1];
+
+	plyFileName = SplitFilename(plyFilePath);
+
 	// read mesh
 	mesh.request_vertex_colors();
 	OpenMesh::IO::Options opt;
@@ -131,8 +176,8 @@ int main(int argc, char** argv)
 	glDepthFunc(GL_LEQUAL);
 
 	// view
-	gluPerspective(80, (double)WINDOW_WIDTH / WINDOW_HEIGHT, 0.1, POINT_NEARZ - POINT_FARZ);
-	gluLookAt(0, 0, POINT_NEARZ,	// eye
+	gluPerspective(80, (double)WINDOW_WIDTH / WINDOW_HEIGHT, 0.1, VIEWPOINT_NEARZ - VIEWPOINT_FARZ);
+	gluLookAt(0, 0, VIEWPOINT_NEARZ,	// eye
 		0, 0 , 0,					// center
 		0, 1, 0);					// Top
 
